ec9b2e29a040f4b2e5dcac68c8d125f9b95a3c85
Kevin Calado <kevincalado96@gmail.com>
2026-06-22 15:57:58 +0000

    nivel aventureiro

--- xadrez.c
+++ xadrez.c
@@ -6,18 +6,21 @@
 // Nível Novato - Movimentação das Peças
 int main() {           
     //declaração de variaveis
-    int rainha, torre = 1, bispo;
+     int rainha, torre = 1, bispo;
+    int cavalo = 1; 
     
     // Implementação de Movimentação do Bispo
      for ( bispo = 0; bispo <= 5; bispo ++) {
         printf(" BISPO: CIMA DIREITA: %d\n", bispo); 
     }
+printf("\n");
 
     // Implementação de Movimentação da Torre
      while (torre <= 5) {
         torre ++;
         printf(" TORRE: DIREITA: %d\n", torre);
     }
+printf("\n");
 
     // Implementação de Movimentação da Rainha
      rainha = 0;
@@ -26,8 +29,16 @@ int main() {
         rainha++;
     } while (rainha <= 8);
 
-
+printf("\n");
     // Nível Aventureiro - Movimentação do Cavalo
+ while (cavalo--)
+   {
+     for( int cavalo1 = 0; cavalo1 < 2; cavalo1++){
+        printf(" CAVALO, CIMA: %d\n", cavalo1);
+     }
+        printf(" CAVALO, ESQUERDA: %d\n", cavalo);
+   }
+
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
 
